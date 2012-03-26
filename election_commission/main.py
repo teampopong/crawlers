82008f5798c4f5e847c7095cd9e434587effd3f8
Cheol Kang <cornchz@gmail.com>
2012-03-27 01:13:29 +0900

    csv 형태로 출력하는 기능

--- election_commission/main.py
+++ election_commission/main.py
@@ -5,13 +5,38 @@ import json
 
 from crawlers import *
 from utils import InvalidTargetError
+from types import UnicodeType
 
 Settings = {
     'START': 1,
-    'END': 19
+    'END': 19,
+    'FILETYPE': 'csv'
 }
 
-def crawl(target, output):
+def print_json(filename, data):
+    with open(filename, 'w') as f:
+        json.dump(cand_list, f, indent=2)
+
+def print_csv(filename, data):
+
+    def transform(txt):
+        txt = txt.replace(',', '|')
+        if isinstance(txt, UnicodeType):
+            txt = txt.encode('utf8')
+        return txt
+
+    attrs = ['district', 'cand_no', 'party', 'name', 'name_cn', 'sex',
+             'birthyear', 'birthmonth', 'birthday', 'address', 'job',
+             'education', 'experience']
+
+    with open(filename, 'w') as f:
+        for cand in data:
+            values = (cand[attr] if attr in cand else '' for attr in attrs)
+            values = (transform(value) for value in values)
+            f.write(','.join(values))
+            f.write('\n')
+
+def crawl(target, printer, filename):
     crawler = None
     if 1 <= target <= 16:
         crawler = CrawlerUntil16()
@@ -27,13 +52,14 @@ def crawl(target, output):
     print 'crawling %d대...' % (target,)
     cand_list = crawler.crawl(target)
 
-    with open(output, 'w') as f:
-        json.dump(cand_list, f, indent=2)
+    printer(filename, cand_list)
 
 def main():
+    printer = print_json if Settings['FILETYPE'] == 'json' else print_csv
+
     for n in xrange(Settings['START'], Settings['END']+1):
-        filename = 'cand-%d.json' % (n,)
-        crawl(target=n, output=filename)
+        filename = 'cand-%d.%s' % (n, Settings['FILETYPE'])
+        crawl(target=n, filename=filename, printer=printer)
 
 if __name__ == '__main__':
     main()
