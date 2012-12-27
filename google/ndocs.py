1ea2dc91d49c219736da983073187830aef5d4d5
Lucy Park <525600min@gmail.com>
2012-12-27 20:22:56 +0900

    Google search index crawler - 성공/실패 여부 출력하기

--- google/ndocs.py
+++ google/ndocs.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-import re, urllib2 
+import re, sys, csv, time, codecs, urllib2 
 import get
 
 settings = {
@@ -10,14 +10,7 @@ settings = {
                 "xpath":'//body//div[@id="resultStats"]'
            }
 
-def as_list(querylist=['박근혜','안철수','문재인']):
-    l = []
-    for q in querylist:
-        l.append(google(q))
-    return l
-
-def as_matrix(querylist=['박근혜','안철수','문재인']):
-    print 'retrieving document numbers from google...'
+def as_matrix(querylist):
     l = len(querylist)
     m = [[0]*l for x in range(l)]
 
@@ -25,10 +18,26 @@ def as_matrix(querylist=['박근혜','안철수','문재인']):
         for j in range(i, l):
             if i==j:
                 q = querylist[i] 
+                run = '\t(%d)\t: %s' % (i,querylist[i])
             else:
                 q = ' '.join([querylist[i], querylist[j]])
-            m[i][j] = google(q)
+                run = '\t(%d,%d)\t: %s,%s' % (i, j, querylist[i], querylist[j])
+            sys.stdout.write(run)
+
+            try:
+                m[i][j] = google(q)
+                stat = 1
+            except:
+                stat = 0
+                pass 
+            print (' - ' + str(stat))
     return m
+
+def as_list(querylist):
+    l = []
+    for q in querylist:
+        l.append(google(q))
+    return l
 
 def google(query):
     url = settings['base_url'] + urllib2.quote(query.encode('utf-8'))
@@ -38,6 +47,20 @@ def google(query):
     n = re.findall(r'[0-9]+', x)
     return int(''.join(n))
 
+def write_csv(attrs, m):
+    filename = 'data/ndocs-%s.csv' % (time.strftime('%Y%m%d%H%M'))
+    attrs = [a.encode('utf-8') for a in attrs]
+    with open(filename, 'wb') as f:
+        #f.write(codecs.BOM_UTF8)
+        w = csv.writer(f)
+        w.writerow(attrs)
+        for i in m:
+            w.writerow(i)
+
 if __name__ == '__main__':
-    QUERYLIST   = ['박은정','강철','윤주희']
-    print(as_matrix(QUERYLIST))
+    QUERYLIST   = ['박근혜','문재인','이정희']
+    print 'retrieving document numbers from google...'
+    m = as_matrix(QUERYLIST)
+    print 'write to file...'
+    write_csv(QUERYLIST, m)
+    print 'done'
