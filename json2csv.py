3c36d2fb3eb5a45848834cdcb0c4ebb36f9d909c
Lucy Park <me@lucypark.kr>
2013-04-17 18:16:38 +0900

    Delete file

--- json2csv.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('cand-0411.json', 'r') as inp, open('output', 'w') as outp:
-    data = json.load(inp, encoding="UTF-8")
-    outp.write(\
-        '\n'.join(\
-            ','.join(\
-                map(lambda x: '"'+x.encode('utf8')+'"', datum.values())\
-                ) for datum in data))
