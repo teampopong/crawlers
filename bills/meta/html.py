aca62cd27590f4497c81b3f0286e029cb5a2cf80
Lucy Park <me@lucypark.kr>
2013-05-22 22:53:12 +0900

    Repetitively request for page if error message is returned

--- bills/meta/html.py
+++ bills/meta/html.py
@@ -13,16 +13,15 @@ import utils
 from settings import BASEURL, DIR, PAGE_SIZE
 
 def convert(assembly_id):
-    url = '%sAGE_FROM=%d&AGE_TO=%d' % (BASEURL['list'], assembly_id, assembly_id)
+    baseurl = '%sAGE_FROM=%d&AGE_TO=%d' % (BASEURL['list'], assembly_id, assembly_id)
     directory = '%s/%s' % (DIR['list'], assembly_id)
-    return url, directory
+    return baseurl, directory
 
 def get_npages(assembly_id):
     url, directory = convert(assembly_id)
 
     fn = '%s/tmp.html' % directory
-    if not os.path.isfile(fn):
-        utils.get_webpage(url, fn)
+    utils.get_webpage(url, fn)
     page = utils.read_webpage(fn)
     m = re.search(u'총(.+)건', page.xpath('//span[@class="text3"]/text()')[0])
     nbills = int(m.group(1))
@@ -31,39 +30,35 @@ def get_npages(assembly_id):
     return npages
 
 def get_html(assembly_id, npages):
-    def get_files(base, page, directory, npages):
+
+    def get_page(baseurl, page, directory, npages):
         try:
-            url = base + '&PAGE=%d&PAGE_SIZE=%d' % (page, PAGE_SIZE)
+            url = baseurl + '&PAGE=%d&PAGE_SIZE=%d' % (page, PAGE_SIZE)
             pn = npages - page + 1
             fn = '%s/%d.html' % (directory, pn)
-            utils.get_webpage(url, fn)
+
+            is_first = True
+            while is_first or 'TEXTAREA ID="MSG" STYLE="display:none"' in doc:
+                r = urllib2.urlopen(url)
+                doc = r.read()
+                is_first = False
+
+            with open(fn, 'w') as f:
+                f.write(doc)
+
             sys.stdout.write('%s\t' % pn)
             sys.stdout.flush()
+
         except (urllib2.URLError, IOError) as e:
             print '\nFailed to get %s due to %s' % (fn, e.__repr__)
 
-    def check_files(directory, npages):
-        files = os.listdir(directory)
-        nums = [int(f.strip('.html')) for f in files if f!='tmp.html']
-        return [m for m in range(1, npages+1) if m not in nums]
-
-    url, directory = convert(assembly_id)
+    baseurl, directory = convert(assembly_id)
     utils.check_dir(directory)
 
     #
     print 'Downloading:'
-    jobs = [gevent.spawn(get_files, url, page, directory, npages)\
+    jobs = [gevent.spawn(get_page, baseurl, page, directory, npages)\
             for page in range(1, npages+1)]
     gevent.joinall(jobs)
-
-    #
-    missing = check_files(directory, npages)
-    if missing:
-        print 'Missing files:'
-        print missing
-        while (missing != []):
-            for m in missing:
-                get_files(url, m, directory, npages)
-            missing = check_files(directory)
 
     return npages
