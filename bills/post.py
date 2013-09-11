ff83a4453c35dc55f8938e9ef1250b9b11f6840e
Cheol Kang <steel@popong.com>
2013-09-11 17:08:59 +0900

    Do not truncate at Facebook

--- bills/post.py
+++ bills/post.py
@@ -16,20 +16,20 @@ INTERVAL_MIN = 5
 INTERVAL_SEC = INTERVAL_MIN * 60
 
 
-def refine_bill_content(bill):
+def refine_bill_content(bill, max_field_len=30):
     # bill_id
     bill_id = bill['bill_id']
 
     # title
     title = bill['title']
-    title = truncate(title, 30)
+    title = truncate(title, max_field_len)
     title_josa = ullul(title.strip('.')[-1])
 
     # proposer
     proposer = bill['status_dict']['접수']['의안접수정보'][0]['제안자']
     while isinstance(proposer, list):
         proposer = proposer[0]
-    proposer = truncate(proposer, 30)
+    proposer = truncate(proposer, max_field_len)
     proposer_josa = yiga(proposer.strip('.')[-1])
 
     return {
@@ -49,7 +49,7 @@ def post_bills_facebook(new_bills):
         post_sentences.append('오늘 발의된 %d개의 새 의안 목록입니다.\n' % (len(new_bills)))
         for idx, bill_id in enumerate(new_bills):
             bill = get_bill(bill_id)
-            bill = refine_bill_content(bill)
+            bill = refine_bill_content(bill, None)
             bill['idx'] = idx
             post_sentences.append('%(idx)d. %(proposer)s. %(title)s\n%(url)s\n' % bill)
         post = "\n".join(post_sentences)
@@ -73,6 +73,9 @@ def post_bill_twitter(bill):
 
 
 def truncate(text, max_len):
+    if max_len is None:
+        return text
+
     if len(text) > max_len:
         text = text[:max_len] + '...'
     return text
