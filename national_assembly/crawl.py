76a4fd155990c17395278a3e823af6c6eac10e98
stray.leone <stray.leone@gmail.com>
2014-03-30 04:46:51 +0900

    update crawler for national assembly

--- national_assembly/crawl.py
+++ national_assembly/crawl.py
@@ -12,9 +12,10 @@ import os
 import re
 import sys
 import urllib2
+from scrapy.selector import Selector # pip install Scrapy
 
 # settings
-PAGE_ENC = 'euc-kr'
+PAGE_ENC = 'utf-8'
 HEADERS = ['name_kr','name_cn','name_en','birth','party','district','committee','when_elected','off_phone','homepage','email','aides','pr_secrs','sc_secrs','hobby','experience','photo','url']
 DATADIR = '.'
 
@@ -51,6 +52,16 @@ def get_page(url, htmldir):
     with open(filename, 'w') as f:
         f.write(page_in_txt)
     return page_in_txt.decode(PAGE_ENC)
+def get_xpath_data(data, _xpath):
+    xpath_selector_list = []
+
+    hxs = Selector(text=data)
+    for i in hxs.xpath(_xpath):
+        xpath_selector_list.append(i.extract().encode("utf-8"))
+    if len(xpath_selector_list) >0 :
+        return  xpath_selector_list[0].decode(PAGE_ENC)
+    else:
+        return xpath_selector_list.decode(PAGE_ENC)
 
 def get_ppl_urls(htmldir):
     def unescape_html(doc):
@@ -60,51 +71,70 @@ def get_ppl_urls(htmldir):
         return doc
 
     url_key = 'people_list'
-    list_class = 'wii_list'
+    list_class = 'memberna_list'
+
 
     page = get_page(urls[url_key], htmldir)
 
     full_member_list = []
-    member_lists = getlist_bracketed_regexp(r'<ul class="%s">(.+?)</ul>' % list_class, page)
+    member_lists = getlist_bracketed_regexp(r'<dd class="img">(.+?)</dd>', page)
+    print len(member_lists)
+
+    #        <dd class="img">
+    #            <a href="#" onclick="jsMemPop(2680)" title="강기윤의원정보 새창에서 열림">
+    #                   <img src="/photo/9770703.jpg" alt="강기윤 의원사진" />
+    #            </a>
+    #        </dd>
+
     for member_list in member_lists:
-        full_member_list += getlist_bracketed_regexp(r'<li class=".*?">[\r\n\s]*?<div class="fl"><a href="(.+?)".*?><img src=".+?" alt="(.+?)".*?>.*?</li>', member_list)
+        full_member_list += getlist_bracketed_regexp(r'<a href="#" onclick="jsMemPop\((.+?)\)".*?>[\r\n\s]*?<img src=".+?" alt="(.+?)".*?/>', member_list)
 
     for url, name in full_member_list:
         url = unescape_html(url)
-        ppl_urls.append(urljoin(urls['base'], url))
+        ppl_urls.append(urls['person']+ url)
 
 def extract_profile(page):
     def parse_name_and_birth(name_and_birth):
-        # name_and_birth example:
-        #        <strong>김윤덕</strong> (金潤德)<br>
-        #        <strong class="txt_s">KIM Yunduk</strong><br>
-        #        <span class="txt_e txt_s">1966.05.23</span>
-        tokens = find_bracketed_texts_regexp(r'<strong>(.+?)</strong>\s\((.+?)\)<br\s*/?>.*?<strong class="txt_s">(.*?)</strong><br\s*/?>.*?<span class="txt_e txt_s">(.*)</span>', name_and_birth)
-        name_kr, name_cn, name_en, birth = tokens
+        #<h4>강기윤</h4>
+        #    <ul>
+        #      <li class="photo">
+        #           <img src="/photo/9770703.jpg" alt="강기윤 의원사진" />
+        #      </li>
+        #      <li>姜起潤</li>
+        #      <li>KANG Gi Yun</li>
+        #      <li>1960-06-04</li>
+        #   </ul>
+        profile = get_xpath_data(page,".//*/div[@class='profile']")
+        name_kr = get_xpath_data(profile, ".//*/h4/text()")
+        name_cn = Selector(text=profile).xpath('.//*/li/text()')[2].extract()
+        name_en = Selector(text=profile).xpath('.//*/li/text()')[3].extract()
+        birth = Selector(text=profile).xpath('.//*/li/text()')[4].extract()
         return [name_kr, name_cn, name_en, birth.replace('.','-')]
 
     # get name & birth
-    name_and_birth = find_bracketed_text_regexp(r'<div class="fl l10 vb t70".*?>(.+?)</div>', page)
-    name_and_birth = parse_name_and_birth(name_and_birth)
+    name_and_birth = parse_name_and_birth(page)
 
     # get experience
-    experience = find_bracketed_text_regexp(r'<ul class="history l05".*?>.*?<li.*?>(.+?)</li>.*?</ul>', page)
-    experience = [d.strip() for d in experience.split('<br>')]
+    experience = find_bracketed_texts_regexp(r'<dl class="per_history">.*?<dd.*?>(.+?)</dd>.*?</dl>', page)
+    experience = ''.join(experience)
+    experience = [d.strip() for d in experience.split('<br />')]
     experience = '||'.join(experience)
 
     # get photo
-    photo = find_bracketed_text_regexp(r'<div class="fl img_box".*?><img src="(.+?)".*?>.*?</div>', page)
+    photo = find_bracketed_text_regexp(r'<li class="photo".*?>[\r\n\s]*?<img src="(.+?)".*?/>[\r\n\s]*?</li>', page)
     photo = urljoin(urls['base'], photo)
 
     # get others
-    others = find_bracketed_text_regexp(r'<table.*?class="view_type03".*?>.*?<tbody>(.+?)</tbody>.*?</table>', page)
-    others = getlist_bracketed_regexp(r'<td.*?>(.*?)</td>', others)
+    others = find_bracketed_text_regexp(r'<dl.*?class="pro_detail">(.+?)</dl>', page)
+    others = getlist_bracketed_regexp(r'<dd>[\r\t\n\s]*?(.+?)[\r\t\n\s]*?</dd>', others)
+    #TODO: I don't know the meaning behind
     try:
         others[5] = re.search(r'<a.*?>(.+?)</a>', others[5]).group(1)
     except AttributeError as e:
         others[5] = ''
 
-    full_profile = list(name_and_birth + others)
+    stripped = [re.sub('[\s\r]+', '', i) for i in others]
+    full_profile = list(name_and_birth + stripped)
     full_profile.append(experience)
     full_profile.append(photo)
     return [p.replace('"',"'") for p in full_profile]
@@ -115,7 +145,7 @@ def crawl_ppl_data(htmldir):
         page = get_page(url, htmldir)
         profile = extract_profile(page)
         ppl_data.append(profile + [url])
-        #print i, ppl_data[i][0]
+        print i, ppl_data[i][0]
 
 def sort_ppl_data(ppl_data):
     ppl_data = sorted(ppl_data, key=lambda x: x[3])
